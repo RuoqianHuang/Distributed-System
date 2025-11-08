6c57cef91af31b2bf0642620dea667705a75c847
cc213 <cc213@fa25-cs425-b601.cs.illinois.edu>
2025-11-08 16:00:03 -0600

    add interactive client

---
+++ MP3/cmd/interactive/main.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"bufio"
+	"cs425/mp3/internal/files"
+	"cs425/mp3/internal/member"
+	"cs425/mp3/internal/menu"
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
+)
+
+const (
+	CONNECTION_TIMEOUT = 1 * time.Second
+	CALL_TIMEOUT       = 1 * time.Second
+)
+
+type Args struct {
+	Command    string
+	Filename   string
+	FileSource string
+	VMAddress  string
+}
+
+func CallWithTimeout(
+	funcName string,
+	hostname string,
+	port int,
+	args any,
+	reply any) error {
+
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", hostname, port), CONNECTION_TIMEOUT)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	client := rpc.NewClient(conn)
+	callChan := make(chan error, 1)
+
+	go func() {
+		callChan <- client.Call(funcName, args, reply)
+	}()
+	err = <-callChan
+	if err != nil {
+		return fmt.Errorf("rpc call to server %s:%d failed: %s", hostname, port, err.Error())
+	}
+	return nil
+}
+
+func readFileName() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter local filename: ")
+	filename, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	filename = strings.TrimSuffix(filename, "\n")
+	filename = strings.TrimSpace(filename)
+	filename, err = filepath.Abs(filename)
+	if err != nil {
+		log.Fatalf("Can't resolve file path: %s", err.Error())
+	}
+	return filename
+}
+
+func displayMember(hostname string, port int) (string, member.Info, error) {
+	infoMap := new(map[uint64]member.Info)
+	err := CallWithTimeout("Server.Member", hostname, port, 0, infoMap)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	table, sortedId := member.CreateTable(*infoMap)
+	options := []string{"exit"}
+	menuMember := menu.NewMenu("Members: ", table, sortedId, options, false)
+	opt, id, err := menuMember.Display()
+	return opt, (*infoMap)[id], err
+}
+
+func displayFiles(hostname string, port int) (string, files.Meta, error) {
+	metaMap := new(map[uint64]files.Meta)
+	err := CallWithTimeout("Server.Files", hostname, port, 0, metaMap)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	table, sortedId := files.CreateTable(*metaMap)
+	options := []string{"exit", "download", "detail"}
+	menuMember := menu.NewMenu("Files: ", table, sortedId, options, true)
+	opt, id, err := menuMember.Display()
+	return opt, (*metaMap)[id], err
+}
+
+func displayBlocks(meta files.Meta) (string, files.BlockInfo, error) {
+	blockMap := make(map[uint64]files.BlockInfo)
+	for i := 0; i < meta.FileBlocks; i++ {
+		blockInfo, _ := meta.GetBlock(i)
+		blockMap[blockInfo.Id] = blockInfo
+	}
+	table, sortedId := files.CreateBlockTable(blockMap)
+	options := []string{"exit", "detail"}
+	menuBlock := menu.NewMenu(fmt.Sprintf("Blocks of %s", meta.FileName), table, sortedId, options, true)
+	opt, id, err := menuBlock.Display()
+	return opt, blockMap[id], err
+}
+
+func displayReplicas(hostname string, port int, blockInfo files.BlockInfo) (string, member.Info, error) {
+	replicas := new([]member.Info)
+	err := CallWithTimeout("Server.GetReplicas", hostname, port, blockInfo.Id, replicas)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	repMap := make(map[uint64]member.Info)
+	for _, replica := range *replicas {
+		reply := new(files.BlockInfo)
+		err := CallWithTimeout("FileManager.ReadBlockInfo", hostname, port, blockInfo.Id, reply)
+		if err == nil {
+			repMap[replica.Id] = replica 
+		}
+	}
+	table, sortedId := member.CreateTable(repMap)
+	prompt := fmt.Sprintf("Replicas of %s block %d:", blockInfo.FileName, blockInfo.BlockNumber)
+	options := []string{"exit", "download"}
+	menuRep := menu.NewMenu(prompt, table, sortedId, options, true)
+	opt, id, err := menuRep.Display()
+	return opt, repMap[id], err
+}
+
+func main() {
+	// handle syscall SIGTERM
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Printf("[Main] SIGTERM, Exiting...")
+		os.Exit(0)
+	}()
+
+	port := 8788
+	hostname := "localhost"
+
+
+	for {
+		options := []string{"members", "files", "exit"}
+		menuOpt := menu.NewMenu("HyDFS Viewer (press enter to select)", "", []uint64{}, options, false)
+		opt, _, err := menuOpt.Display()
+		if err != nil || opt == "exit" {
+			break
+		}
+		if opt == "members" {
+			displayMember(hostname, port)
+		} else {
+			opt, meta, err := displayFiles(hostname, port)
+			if err == nil && opt != "exit" {
+				if opt == "download" {
+					filename := readFileName()
+					args := Args{
+						Command: "get",
+						Filename: meta.FileName,
+						FileSource: filename,
+					}
+					reply := new(string)
+					err = CallWithTimeout("Server.CLI", hostname, port, args, reply)
+					if err != nil {
+						log.Fatal(err.Error())
+					} else {
+						log.Print(*reply)
+					}
+					break
+				} else {
+					// Detail
+					for {
+						opt, blockInfo, err := displayBlocks(meta)
+						if err != nil || opt == "exit" {
+							break
+						} 
+						opt, replica, _ := displayReplicas(hostname, port, blockInfo)
+						if err == nil && opt == "download" {
+							filename := readFileName()
+							blockPack := new(files.BlockPackage)
+							err := CallWithTimeout("FileManager.ReadBlock", replica.Hostname, replica.Port + 1, blockInfo.Id, blockPack)
+							if err != nil {
+								log.Fatal(err.Error())
+							}
+							// write block to local file
+							err = os.WriteFile(filename, blockPack.Data, 0644)
+							if err != nil {
+								log.Fatalf("Failed to downbload %s block %d: %s", blockInfo.FileName, blockInfo.BlockNumber, err.Error())
+							} else {
+								log.Printf("Block %d of %s download to %s successfully!", blockInfo.BlockNumber, blockInfo.FileName, filename)
+							}
+							return
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ MP3/internal/menu/menu.go
@@ -0,0 +1,274 @@
+package menu
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"github.com/buger/goterm"
+	"github.com/pkg/term"
+)
+
+// Terminal control codes
+const (
+	ShowCursor = "\033[?25h"
+	HideCursor = "\033[?25l"
+	// CursorUpFormat Requires formatting with number of lines
+	CursorUpFormat = "\033[%dA"
+	ClearLine      = "\r\033[K"
+	KeyUp          = byte(65)
+	KeyDown        = byte(66)
+	KeyLeft        = byte(68)
+	KeyRight       = byte(67)
+	KeyEscape      = byte(27)
+	KeyEnter       = byte(13)
+	KeyTab         = byte(9) // Tab 鍵
+)
+
+// NavigationKeys
+var NavigationKeys = map[byte]bool{
+	KeyUp:    true,
+	KeyDown:  true,
+	KeyLeft:  true,
+	KeyRight: true,
+}
+
+// MenuContext Mode
+type MenuContext int
+
+const (
+	MenuContextOptions MenuContext = iota // Focus on options
+	MenuContextTable                      // Focus on table
+)
+
+
+type Menu struct {
+	Prompt string
+
+	// Options (Horizontal Menu)
+	Options      []string
+	OptionCursor int
+
+	// Table (Vertical Menu)
+	SortedId     []uint64
+	TableLines   []string
+	TableCursor  int
+
+	// State
+	ActiveContext MenuContext
+	SelectTable   bool
+
+	// Display partial rows
+	DisplayRows  int
+	StartingRow  int
+}
+
+func NewMenu(prompt string, tableContent string, sortedId []uint64, options []string, selectTable bool) *Menu {	
+	if len(sortedId) > 0 {
+		tablelines := strings.Split(tableContent, "\n") // Table 
+		if len(tablelines) > 0 && tablelines[len(tablelines)-1] == "" {
+            tablelines = tablelines[:len(tablelines)-1] 
+        }
+		return &Menu{
+			Prompt:        prompt,
+			Options:       options,                        // Options
+			SortedId: sortedId,
+			TableLines: tablelines,
+			ActiveContext: MenuContextOptions,             // Default on options
+			SelectTable: selectTable,
+			DisplayRows: min(len(tablelines) - 4, 10),
+		}
+	} 
+	return &Menu{
+		Prompt:        prompt,
+		Options:       options,                        // Options
+		ActiveContext: MenuContextOptions,             // Default on options
+		SelectTable: false,
+	}
+}
+
+func (m *Menu) renderUI(redraw bool) {
+	// Render the whole UI; if redraw=true, move cursor to top
+
+    if redraw {
+        // Calculate total line count
+		tableLines := 0
+		if m.TableLines != nil {
+			tableLines = m.DisplayRows + 4 // Row + header(3) + footer(1)
+		}
+        totalLines := tableLines + 2
+        
+        // Move the cursor to the top
+        fmt.Printf(CursorUpFormat, totalLines)
+    }
+
+	// Print Prompt
+	fmt.Printf("%s\n", goterm.Color(goterm.Bold(m.Prompt)+":", goterm.CYAN))
+    
+
+	// Print Options
+	optionLine := ""
+	for i, opt := range m.Options {
+		if i == m.OptionCursor {
+			optionLine += goterm.Color(fmt.Sprintf("   >%s   ", opt), goterm.YELLOW)
+		} else {
+			optionLine += fmt.Sprintf("    %s    ", opt)
+		}
+	}
+	
+    fmt.Print(ClearLine) // Clear current line
+	if m.ActiveContext == MenuContextOptions {
+		fmt.Print(goterm.Background(optionLine, goterm.BLUE) + "\n")
+	} else {
+		fmt.Print(optionLine + "\n")
+	}
+
+
+	if m.TableLines != nil {
+		// Render table
+		headerLines := 3
+		
+		// Calculate StartingRow
+		if m.TableCursor < m.StartingRow {
+			m.StartingRow = m.TableCursor
+		}
+		if m.TableCursor >= m.StartingRow + m.DisplayRows {
+			m.StartingRow = m.TableCursor - m.DisplayRows + 1
+		}
+		
+
+		dataLines := m.TableLines[headerLines + m.StartingRow : headerLines + m.StartingRow + m.DisplayRows]
+		numDataRows := len(dataLines)
+		
+		// Print the header
+		fmt.Print(ClearLine + m.TableLines[0] + "\n")
+		fmt.Print(ClearLine + m.TableLines[1] + "\n")
+		fmt.Print(ClearLine + m.TableLines[2] + "\n")
+		
+		// Print rows
+		termHeight := goterm.Height()
+		for i := 0; i < min(termHeight, numDataRows); i++ {
+			line := dataLines[i]
+			fmt.Print(ClearLine) // Clear current line
+			if i == m.TableCursor - m.StartingRow && m.ActiveContext == MenuContextTable {
+				fmt.Print(goterm.Color(line, goterm.YELLOW) + "\n") // Highlight current line
+			} else {
+				fmt.Print(line + "\n")
+			}
+		}
+
+		// Print the footer
+		fmt.Print(ClearLine + m.TableLines[len(m.TableLines) - 1] + "\n")
+	}
+
+	goterm.Flush()
+}
+
+func (m *Menu) Display() (string, uint64, error) {
+	defer func() {
+		// restore cursor
+		fmt.Print(ShowCursor)
+		goterm.Clear() 
+		goterm.MoveCursor(1, 1)
+		goterm.Flush()
+	}()
+	
+	// first render
+	m.renderUI(false)
+	fmt.Print(HideCursor)
+
+	// deal with SIGTERM
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGTERM)
+
+	for {
+		keyCode := getInput()
+		numDataRows := len(m.SortedId)
+
+		switch keyCode {
+		case KeyEscape: // Exit
+			return "", 0, fmt.Errorf("exit")
+		
+		case KeyTab: // switch context
+			if m.ActiveContext == MenuContextOptions && m.SelectTable {
+				m.ActiveContext = MenuContextTable
+			} else {
+				m.ActiveContext = MenuContextOptions
+			}
+
+		case KeyLeft:
+			if m.ActiveContext == MenuContextOptions {
+				m.OptionCursor = (m.OptionCursor + len(m.Options) - 1) % len(m.Options)
+			}
+
+		case KeyRight:
+			if m.ActiveContext == MenuContextOptions {
+				m.OptionCursor = (m.OptionCursor + 1) % len(m.Options)
+			}
+
+		case KeyUp:
+			if m.ActiveContext == MenuContextTable && numDataRows > 0 {
+				m.TableCursor = (m.TableCursor + numDataRows - 1) % numDataRows
+			}
+
+		case KeyDown:
+			if m.ActiveContext == MenuContextTable && numDataRows > 0 {
+				m.TableCursor = (m.TableCursor + 1) % numDataRows
+			}
+		
+		case KeyEnter: // selection
+			if m.ActiveContext == MenuContextOptions && m.SelectTable {
+				// If press enter on options, switch to table
+				m.ActiveContext = MenuContextTable
+			} else if m.ActiveContext == MenuContextTable && numDataRows > 0 {
+				// If press enter on table, return selected value
+				selectedOption := m.Options[m.OptionCursor]
+				selectedRowID := m.SortedId[m.TableCursor]
+				goterm.Clear()
+				goterm.MoveCursor(1, 1)
+				goterm.Flush()
+				fmt.Print(ShowCursor)
+				return selectedOption, selectedRowID, nil
+			} else if !m.SelectTable {
+				selectedOption := m.Options[m.OptionCursor]
+				goterm.Clear()
+				goterm.MoveCursor(1, 1)
+				goterm.Flush()
+				fmt.Print(ShowCursor)
+				return selectedOption, 0, nil
+			}
+		}
+		m.renderUI(true)
+	}
+}
+
+func getInput() byte {
+	t, _ := term.Open("/dev/tty")
+	defer t.Close()
+
+	err := term.RawMode(t)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var read int
+	readBytes := make([]byte, 3)
+	read, err = t.Read(readBytes)
+	if err != nil {
+		return 0
+	}
+
+	defer t.Restore() 
+
+	if read == 3 {
+		if _, ok := NavigationKeys[readBytes[2]]; ok {
+			return readBytes[2]
+		}
+	} else {
+		return readBytes[0]
+	}
+
+	return 0
+}
