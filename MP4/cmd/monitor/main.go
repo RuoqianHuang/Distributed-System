958e8412ff1daebfcebe9b501044202ec4706e0e
cc213 <cc213@fa25-cs425-b601.cs.illinois.edu>
2025-12-06 00:47:23 -0600

    add monitor for watching flow workers

---
+++ MP4/cmd/monitor/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"cs425/mp4/internal/leader"
+	"cs425/mp4/internal/member"
+	"cs425/mp4/internal/utils"
+	"log"
+)
+
+
+func main() {
+	reply := new(map[uint64]member.Info)
+	err := utils.RemoteCall("Leader.GetMember", "localhost", leader.RAINSTORM_LEADER_PORT_RPC, false, reply)
+	if err != nil {
+		log.Fatalf("Fail to get member info: %v", err)
+	}
+	table, _ := member.CreateTable(*reply)
+	log.Printf("%s", table)
+}
